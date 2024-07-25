0291885bf4685e8bcfa24a671e0ef818619bc216
kumar <tinytachyon14341@gmail.com>
2024-07-25 23:07:40 +0530

    base model component

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from renal_tumor_detection import logger
 from renal_tumor_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from renal_tumor_detection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")  
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare base model stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")  
 
