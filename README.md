a56a31ecbeee198f432a18d83092f37f5e6d3cf2
kumar <tinytachyon14341@gmail.com>
2024-07-25 16:03:43 +0530

    data ingestion added

---
+++ README.md
@@ -0,0 +1,17 @@
+# END-TO-END DEEP LEARNING PROJECT
+
+## Workflows
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline 
+8. Update the main.py
+9. Update the dvc.yaml
+10. app.py
+
+
+## Dataset : https://drive.google.com/file/d/1Mg6Gv7-sPl54NUL9Jx4h9_2dI943W4Ii/view?usp=sharing
---
+++ main.py
@@ -0,0 +1,14 @@
+from renal_tumor_detection import logger
+from renal_tumor_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")  
+
+except Exception as e:
+    logger.exception(e)
+    raise e
