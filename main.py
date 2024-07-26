7dc3ce7b13cd85d762487dc1bc80ae48f2ac7851
kumar <tinytachyon14341@gmail.com>
2024-07-26 18:12:05 +0530

    model trainer component

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from renal_tumor_detection import logger
 from renal_tumor_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from renal_tumor_detection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from renal_tumor_detection.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -20,6 +20,18 @@ try:
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")  
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"******************************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
 except Exception as e:
     logger.exception(e)
