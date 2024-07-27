c3fd963c1dd0aa427ae6b1d80890f113a2d6bb1a
kumar <tinytachyon14341@gmail.com>
2024-07-27 23:20:51 +0530

    flask application added

---
+++ src/renal_tumor_detection/pipeline/prediction.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+class PredictionPipeline:
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def predict(self):
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
+        
+        imagename = self.file_name
+        test_image = image.load_img(imagename, target_size = (224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+    
+        if result[0] == 1:
+            prediction = "Tumor"
+            return [{"image": prediction}]
+        else:
+            prediction = "No Tumor"
+            return [{"image": prediction}]
